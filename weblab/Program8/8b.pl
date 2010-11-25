d8d34e54d6e1ffa76cbc5bba5f72894b42062f9a
Kunal Ghosh <kunal.t2@gmail.com>
2010-11-24 23:33:08 -0500

    Program 9 Finished

---
+++ weblab/Program8/8b.pl
@@ -0,0 +1,12 @@
+#!/usr/bin/perl
+
+print "Content-type:text/html\n\n";
+
+open(IN,'<visitor.txt');
+$cnt = <IN>;
+close(IN);
+$cnt++;
+open(OUT,'>visitor.txt');
+print OUT $cnt;
+close(OUT);
+print "You are visitor number : $cnt";
