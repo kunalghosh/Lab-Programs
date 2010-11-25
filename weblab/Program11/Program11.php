c2665ac7508b1d74cebc904a76ba86ee239c49e1
Kunal Ghosh <kunal.t2@gmail.com>
2010-11-25 01:06:46 -0500

    Modified Program 11

--- weblab/Program11/Program11.php
+++ weblab/Program11/Program11.php
@@ -5,7 +5,9 @@ if (isset($_COOKIE["visited"])){
 	$value=$_COOKIE["visited"]; 
 	print "Last Visited on: $value"; 
 	}
+else{
 setcookie("visited",date("G:i:s a - m/d/y"),"$expiry");
-
+print "NEW COOKIE SET !";
+}
 
 ?>
